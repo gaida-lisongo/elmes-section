2d1f549b2ea7cf14548990a424eaf186242953bf
gaida-lisongo <inbtpkinshasa@gmail.com>
2025-08-19 21:23:08 +0100

    merged ok

--- index.js
+++ index.js
@@ -71,32 +71,3 @@ app.use("/api", routes);
 app.listen(PORT, () => {
   console.log(`Serveur démarré sur le port ${PORT}`);
 });
-
-const express = require("express");
-const jwt = require("jsonwebtoken");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const routes = require("./router");
-const db = require("./service/Database");
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// Routes
-app.use("/api", routes);
-
-// Démarrage du serveur
-app.listen(PORT, () => {
-  console.log(`Serveur démarré sur le port ${PORT}`);
-});
